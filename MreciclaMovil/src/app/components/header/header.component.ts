bb3ba97c84c31ea94943083fbd8e87a8cf3325f1
LudwingZayas <ludwingzayas@gmail.com>
2024-01-03 17:51:55 -0600

    modificación de catalogo

--- MreciclaMovil/src/app/components/header/header.component.ts
+++ MreciclaMovil/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',

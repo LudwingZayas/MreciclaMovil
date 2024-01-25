cab6f94270257b69225a823e3933c383c9f2d8a2
LudwingZayas <ludwingzayas@gmail.com>
2024-01-25 17:56:39 -0600

    nuevas mejoras para los tiempos de producción

--- MreciclaMovil/src/app/components/tiempos-produccion/tiempos-produccion-crear/tiempos-produccion-crear.component.ts
+++ MreciclaMovil/src/app/components/tiempos-produccion/tiempos-produccion-crear/tiempos-produccion-crear.component.ts
@@ -3,16 +3,19 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { NavController } from '@ionic/angular';
+import { TiemposProduccionCrearService } from './tiempos-produccion-crear.service';
 
 @Component({
   selector: 'app-tiempos-produccion-crear',
   templateUrl: './tiempos-produccion-crear.component.html',
   styleUrls: ['./tiempos-produccion-crear.component.scss'],
 })
-export class TiemposProduccionCrearComponent  implements OnInit {
+export class TiemposProduccionCrearComponent  implements OnInit
+{
 
   
-
+  horaInicio: string | undefined;
+  horaFin: string | undefined;
   botonHabilitado = true;
   
 
@@ -47,14 +50,57 @@ export class TiemposProduccionCrearComponent  implements OnInit {
 
   formulario: FormGroup;
 
-  constructor(private router:Router,private formBuilder: FormBuilder,private navCtrl: NavController ) {
+  constructor(private router:Router,
+    private formBuilder: FormBuilder,private navCtrl: NavController,private http: HttpClient,
+    private tiemposProduccionCrearService:TiemposProduccionCrearService
+    
+    ) {
+   
+   
+   
+   
+   
+   
+   
     this.formulario = this.formBuilder.group({
       horaInicio: ['']
     });
 
+
+
+
+
+
+
+
+
+
+    
     // Establecer la hora actual como valor predeterminado
     const horaActual = new Date().toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: false });
     this.formulario.controls['horaInicio'].setValue(horaActual);
+  }
+
+  registrarInicio() {
+    this.tiemposProduccionCrearService.registrarHoraInicio().subscribe(
+      response => {
+        console.log(response); // Maneja la respuesta del servidor
+      },
+      error => {
+        console.error(error); // Maneja errores
+      }
+    );
+  }
+
+  registrarSalida() {
+    this.tiemposProduccionCrearService.registrarHoraSalida().subscribe(
+      response => {
+        console.log(response); // Maneja la respuesta del servidor
+      },
+      error => {
+        console.error(error); // Maneja errores
+      }
+    );
   }
 
   irtiemposincidencias(){
---
+++ MreciclaMovil/src/app/components/tiempos-produccion/tiempos-produccion-crear/tiempos-produccion-crear.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TiemposProduccionCrearService {
+  private apiUrl = 'http://localhost/PhpAngular/MreciclaMovil/horarios.php';
+
+  constructor(private http: HttpClient) {}
+  
+
+  registrarHoraInicio() {
+    return this.http.post(`${this.apiUrl}`, { accion: 'inicio' });
+  }
+
+  registrarHoraSalida() {
+    return this.http.post(`${this.apiUrl}`, { accion: 'salida' });
+  }
+}
